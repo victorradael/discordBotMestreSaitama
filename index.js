42de1cc7ac944427eaf7f81691e00050965beb19
Victor Radael <victorradael@hotmail.com>
2021-03-15 20:06:15 -0300

    :robot: Host

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const bot = new Discord.Client();
 
 const token = process.env.BOT_TOKEN;
 
+//heroku
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (request, response) => response.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 bot.login(token);
 bot.on("ready", () => {
   console.log("Mestre Saitama Online!");
